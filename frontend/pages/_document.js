3615b0aed235d1fb7520784efd444cea789d7249
Dell <Dell@DESKTOP-G0P8UD6>
2026-09-17 10:24:21 +0545

    Favicon: real multi-size favicon.ico + PNGs (fixes blank tab icon; SVG-with-external-image does not render)

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,9 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="icon" href="/logo.webp" type="image/webp" />
-        <link rel="apple-touch-icon" href="/logo.webp" />
+        <link rel="icon" href="/favicon.ico" sizes="48x48" />
+        <link rel="icon" href="/icon-32.png" type="image/png" sizes="32x32" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
